9c4874b233a80cca9c38a2b7508e27d088e455be
Leevi Kukkonen <K8397@student.jamk.fi>
2020-05-18 21:54:53 +0300

    new homepage. Maybe this one comes out good...

---
+++ homepage/homepagev3/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Container, Typography, Tabs, Tab } from "@material-ui/core/";
+import "./App.css";
+
+function App() {
+	return (
+		<Container className="App">
+			<Typography>Hello world</Typography>{" "}
+			<Tabs>
+				<Tab label="Item One" />
+				<Tab label="Item Two" />
+				<Tab label="Item Three" />
+			</Tabs>
+		</Container>
+	);
+}
+
+export default App;
