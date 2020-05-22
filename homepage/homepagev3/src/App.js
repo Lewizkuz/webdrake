11ddaa6767f4a3cca966a9043e82fa1b52870b7a
Leevi Kukkonen <K8397@student.jamk.fi>
2020-05-22 19:45:49 +0300

    Added tabs styling, components, and theme skeleton

--- homepage/homepagev3/src/App.js
+++ homepage/homepagev3/src/App.js
@@ -2,30 +2,50 @@ import React from "react";
 import {
 	Container,
 	Typography,
-	Tabs,
-	Tab,
-	withStyles,
-	createStyles,
+	makeStyles,
+	createMuiTheme,
+	ThemeProvider,
 } from "@material-ui/core/";
+import { orange } from "@material-ui/core/colors";
+import NavBar from "./Components/navbar";
 import "./App.css";
 
-const theme = createStyles({
-	root: {
-		display: "flex",
+const theme = createMuiTheme({
+	status: {
+		danger: orange[500],
+	},
+	palette: {
+		primary: {
+			main: orange[400],
+		},
 	},
 });
 
+const useStyles = makeStyles((theme) => ({
+	root: {
+		color: theme.palette.primary,
+	},
+	navbar: {
+		color: theme.status.danger,
+	},
+}));
+const tabs = ["About", "Introduction", "Examples"];
 const App = () => {
+	const classes = useStyles();
 	return (
-		<Container accent className="App">
-			<Typography>Hello world</Typography>{" "}
-			<Tabs>
-				<Tab label="Item One" />
-				<Tab label="Item Two" />
-				<Tab label="Item Three" />
-			</Tabs>
+		<Container className={classes.root}>
+			<Typography className={classes.root} align="center" variant="h1">
+				Hello world
+			</Typography>
+			<NavBar class={classes.navbar} tabheads={tabs} startindex={1}></NavBar>
 		</Container>
 	);
 };
 
-export default withStyles(App);
+export default () => {
+	return (
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
+	);
+};
