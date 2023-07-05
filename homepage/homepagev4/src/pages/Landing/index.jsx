b77c7edfb404ffdbd5188a8e43b41d0020c089d7
Qukkonen <leevi.kukkonen@tridea.fi>
2023-07-05 18:12:07 +0300

    Worked on landing hover handling

--- homepage/homepagev4/src/pages/Landing/index.jsx
+++ homepage/homepagev4/src/pages/Landing/index.jsx
@@ -1,11 +1,40 @@
+import { useState } from "react";
+import { PropTypes } from "prop-types";
+import "./landing.css";
+const DynamicImage = ({ url, alt }) => (
+  <img src={url} alt={alt || "dynamicImages"} />
+);
+DynamicImage.propTypes = {
+  url: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+};
+
 export const Landing = () => {
+  const [toggleImage, setToggleImage] = useState("neutral");
+  const handleImageToggle = (setValue) => () => {
+    setToggleImage(setValue);
+  };
   return (
-    <content>
-      <article>
-        <h1>Leevi Kukkonen stuff</h1>
-        <a href={window.location.href + "tech"}>Tech page</a>
-        <a href={window.location.href + "art"}>Art page</a>
-      </article>
-    </content>
+    <main className="landing-main">
+      <div>
+        <DynamicImage url={toggleImage} alt={toggleImage} />
+      </div>
+      <a
+        href={window.location.href + "tech"}
+        onMouseOver={handleImageToggle("tech")}
+        onMouseLeave={handleImageToggle("netural")}
+      >
+        Tech
+      </a>
+      <a
+        href={window.location.href + "art"}
+        onMouseOver={handleImageToggle("art")}
+        onMouseLeave={handleImageToggle("netural")}
+      >
+        Art
+      </a>
+
+      <p>@Leevi Kukkonen 2023</p>
+    </main>
   );
 };
